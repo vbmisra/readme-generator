60c2eb1e66f25eadcc8cd851ef120aee84192d6f
Invincible <87626336+vbmisra@users.noreply.github.com>
2021-11-14 17:38:18 -0800

    committing changes to index file and readme

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const promptUser = () => {
       type: "checkbox",
       name: "license",
       message: "Which license applies to your project?",
-      choices: ["phone", "message", "email"],
+      choices: ["MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)", "Boost [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)", "Eclipse [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"],
     },
     {
       type: "input",
@@ -95,78 +95,4 @@ const init = () => {
 };
 
 init();
-
-// inquirer
-//   .prompt([
-//     {
-//       type: "input",
-//       message: "What is your name?",
-//       name: "name",
-//     },
-//     {
-//       type: "input",
-//       message: "Where do you live?",
-//       name: "location",
-//     },
-//     {
-//       type: "input",
-//       message: "Tell us about yourself:",
-//       name: "bio",
-//     },
-//     {
-//         type: "input",
-//         message: "What is your linkedin URL?",
-//         name: "linkedInURL",
-//     },
-//     {
-//         type: "input",
-//         message: "What is your github URL?",
-//         name: "githubURL",
-//     },
-//   ])
-//   .then((response) => {
-//     // let choicesStr = response.name.join(",")
-//     // let msg = `My Names is ${response.name} I write ${response.location}, and my prefered method of communications are: ${response.bio}, ${response.linkedInURL}, and ${response.githubURL} \n`
-//     // let msg = `<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">\n<title>Portfolio</title>\n
-//     // </head>\n<body>\n<div class="card">\n<div class="card-body">\n ${response.name}\n</div>\n<div class="card-body">\n ${response.location} \n            </div> \n            <div class="card-body">\n                ${response.bio}\n            </div>\n            <div class="card-body">\n                ${response.linkedInURL}\n            </div>\n
-//     //             <div class="card-body">\n                ${response.githubURL}\n            </div>\n        </div>\n    </body>\n    </html>`
-//     let msg = 
-//     `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-//     <title>Portfolio</title>
-// </head>
-// <body>
-//     <div class="card">
-//         <div class="card-body">
-//           ${response.name}
-//         </div>
-//         <div class="card-body">
-//           ${response.location}
-//         </div>
-//         <div class="card-body">
-//           ${response.bio}
-//         </div>
-//         <div class="card-body">
-//           ${response.linkedInURL}
-//         </div>
-//         <div class="card-body">
-//           ${response.githubURL}
-//         </div>
-//     </div>
-// </body>
-// </html>`
-//     fs.writeFile(
-//       "portfolio.html",
-//       msg,
-//       (err) => {
-//         if (err) throw err;
-//         console.log("Success! Saved!");
-//       }
-//     );
-//   });
 
